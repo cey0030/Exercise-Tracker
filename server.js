fb7ad119d2ddb218e0b1a47be979c8f18a822c93
Icy <48426058+cey0030@users.noreply.github.com>
2020-06-23 13:21:14 +1200

    Secured connection url

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const cors = require('cors')
 
 const mongoose = require('mongoose')
 
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
+
 mongoose
   .connect(
-    'mongodb://Clement:clement@cluster0-shard-00-00-yghgs.mongodb.net:27017,cluster0-shard-00-01-yghgs.mongodb.net:27017,cluster0-shard-00-02-yghgs.mongodb.net:27017/Cluster0?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority' || 'mongodb://localhost/exercise-track',
-    { useNewUrlParser: true }
+    process.env.CONNECTION_URL || 'mongodb://localhost/exercise-track',
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
